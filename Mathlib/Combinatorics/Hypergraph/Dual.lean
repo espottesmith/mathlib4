f853f4af410570e0f58b4e737292d46a5adbdc0b
Evan Walter Clark Spotte-Smith, PhD <espottesmith@gmail.com>
2025-08-22 14:15:17 -0400

    Dual, slash giving up on (part of) dual

--- Mathlib/Combinatorics/Hypergraph/Dual.lean
+++ Mathlib/Combinatorics/Hypergraph/Dual.lean
@@ -14,48 +14,38 @@ variable {H : Hypergraph α}
 @[simps]
 def dual (H : Hypergraph α) (f : Set α → β) : Hypergraph β where
   vertexSet := E(H).image f
-  hyperedgeSet := V(H).image fun x ↦ {e ∈ E(H) | x ∈ e}.image f
+  hyperedgeSet := V(H).image fun x ↦ (H.star x).image f
   hyperedge_isSubset_vertexSet' e he := by
     simp only [Set.mem_image] at he
     obtain ⟨v, hv, h⟩ := he
     rw [← h]
     refine image_mono ?_
-    exact sep_subset E(H) fun x ↦ v ∈ x
+    exact sep_subset E(H) fun e ↦ v ∈ e
+
+@[simps]
+def dual' (H : Hypergraph α) : Hypergraph (Set α) where
+  vertexSet := E(H)
+  hyperedgeSet := V(H).image fun x ↦ H.star x
+  hyperedge_isSubset_vertexSet' e he := by
+    simp only [Set.mem_image] at he
+    obtain ⟨v, hv, h⟩ := he
+    rw [← h]
+    exact sep_subset E(H) fun e ↦ v ∈ e
 
 scoped notation H "*" f => Hypergraph.dual H f
 
-@[simp]
-lemma mem_dual {f : Set α → β} {H : Hypergraph α} :
-    X ∈ E(H.dual f) ↔ ∃ x ∈ V(H), {e ∈ E(H) | x ∈ e}.image f = X := by apply Set.mem_image
+scoped notation H "*'" => Hypergraph.dual H
 
 @[simp]
-lemma dual_dual {H : Hypergraph α} {f : Set α → β} {f' : Set β → α} {e : Set α}
-  (h : ∀ x ∈ V(H), f' ((H.star x).image f) = x)
-  (hf : f.Injective)
-  : (H.dual f).dual f' = H := by
-    expose_names
-    refine Hypergraph.ext ?_ ?_
-    · simp
-      grind
-    · refine ext ?_
-      intro e
-      let E := (f e)
-      have h' : e ∈ E(H) ↔ E ∈ V(H*f) := by
-        simp
-        constructor
-        · intro hmem
-          use e
-        · intro hfe
-          obtain ⟨e', he⟩ := hfe
-          have he' : E = f e' := by exact Eq.symm he.2
-          have hee : e = e' := by exact hf he'
-          grind
-      have h'' : E ∈ V(H*f) ↔ e ∈ E((H*f)*f') := by
-        constructor
-        · intro hmemE
-          sorry
-        · intro hmeme
-          sorry
-      exact Iff.trans (id (Iff.symm h'')) (id (Iff.symm h'))
+lemma mem_dual {f : Set α → β} {H : Hypergraph α} :
+    X ∈ E(H.dual f) ↔ ∃ x ∈ V(H), (H.star x).image f = X := by apply Set.mem_image
+
+@[simp]
+lemma mem_dual' {E : Set (Set α)} :
+    E ∈ E(H.dual') ↔ ∃ x ∈ V(H), H.star x = E := by apply Set.mem_image
+
+-- TODO: (H*)* = H
+-- Neither definition seems amenable to this proof
+-- Possible case for Hypergraph α β instead of Hypergraph α
 
 end Hypergraph
