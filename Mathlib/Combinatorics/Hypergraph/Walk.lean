c463fb9b651edb00cb88397e5f6ecfe0527ecb6c
Evan Walter Clark Spotte-Smith, PhD <espottesmith@gmail.com>
2025-09-12 14:54:37 +0100

    Minor update (broken)

--- Mathlib/Combinatorics/Hypergraph/Walk.lean
+++ Mathlib/Combinatorics/Hypergraph/Walk.lean
@@ -31,45 +31,69 @@ namespace Hypergraph
 
 variable {H : Hypergraph α}
 
-def IsWalk (H : Hypergraph α) : List (α × Set α) → Prop
-  | [] => true
-  | (_, e) :: [] => e = ∅
-  | (x, e) :: (y, e') :: W => e ∈ E(H) ∧ x ∈ e ∧ y ∈ e ∧ x ≠ y ∧ H.IsWalk ((y, e') :: W)
-
-
-@[ext]
-structure WalkStruct (α : Type*) where
-  -- The hypergraph on which this walk exists
-  H : Hypergraph α
-  -- The steps of the walk. The starting point (final element) must have associated edge ∅
-  flags : List (α × Set α)
-  -- The flags must form a valid sequence of adjacency-based steps
-  flags_isWalk : H.IsWalk flags
-
-inductive Walk : α → α → Type u
-  | nil {x : α} : Walk x x
-  | cons {x y z : α} {e : Set α} (he : e ∈ E(H))
-    (hx : x ∈ e) (hy : y ∈ e) (hxy : x ≠ y) (p : Walk y z) : Walk x z
-
-instance : DecidableEq Walk
-
-attribute [refl] Walk.nil
+inductive IsWalk (H : Hypergraph α) : α → List (α × Set α) → Prop
+  | nil : IsWalk H _ []
+  | cons : ∀ {x y : α} {e : Set α} {W : List (α × Set α)}, e ∈ E(H) → x ∈ e → y ∈ e → x ≠ y →
+      IsWalk H x W → IsWalk H y (.cons ⟨y, e⟩ W)
 
 @[simps]
-instance Walk.instInhabited (x : α) : Inhabited (H.Walk x x) := ⟨Walk.nil⟩
+instance IsWalk.instInhabited (x : α) : Inhabited (H.IsWalk x []) := ⟨IsWalk.nil⟩
 
-namespace Walk
+namespace IsWalk
 
-/-- Pattern to get `Walk.nil` with the vertex as an explicit argument. -/
+/-- Pattern to get `IsWalk.nil` with the vertex as an explicit argument. -/
 @[match_pattern]
-abbrev nil' (x : α) : H.Walk x x := Walk.nil
+abbrev nil' (x : α) : H.IsWalk x [] := IsWalk.nil
 
-/-- Pattern to get `Walk.cons` with the vertices and connecting edge as explicit arguments. -/
+/-- Pattern to get `IsWalk.cons` with the vertex, edge, and step list as explicit arguments. -/
 @[match_pattern]
-abbrev cons' (x y z : α) (e : Set α) (he : e ∈ E(H)) (hx : x ∈ e) (hy : y ∈ e) (hxy : x ≠ y)
-  (p : H.Walk y z) : H.Walk x z := Walk.cons he hx hy hxy p
+abbrev cons' (x y : α) (e : Set α) (W : List (α × Set α))
+  (he : e ∈ E(H)) (hx : x ∈ e) (hy : y ∈ e) (hxy : x ≠ y) (p : H.IsWalk x W) :
+  H.IsWalk y ((y, e) :: W) := IsWalk.cons he hx hy hxy p
 
-end Walk
+/--
+Change the vertices and steps of a walk using equalities. This is helpful for relaxing
+definitional equality constraints and to be able to state otherwise difficult-to-state
+lemmas. While this is a simple wrapper around `Eq.rec`, it gives a canonical way to write it.
+
+The simp-normal form is for the `copy` to be pushed outward. That way calculations can
+occur within the "copy context."
+
+Credit: Kyle Miller
+-/
+protected def copy {x x' W W'} (p : H.IsWalk x W) (hx : x = x') (hW : W = W') : H.IsWalk x' W' :=
+  hx ▸ hW ▸ p
+
+/-- The length of a walk is the number of edges/darts along it. -/
+-- TODO: you are here
+-- Not sure why this isn't working...
+def length {x : α} {W :  List (α × Set α)} : H.IsWalk x W → ℕ
+  | nil => 0
+  | cons _ _ _ _ _ => W.length
+
+-- /-- The `support` of a walk is the list of vertices it visits in order. -/
+-- def support {x : α} : H.IsWalk x W → List α
+--   | nil => [x]
+--   | cons _ _ _ _ p => x :: p.support
+
+-- /-- The `darts` of a walk is the list of darts it visits in order. -/
+-- def darts {u v : V} : G.Walk u v → List G.Dart
+--   | nil => []
+--   | cons h p => ⟨(u, _), h⟩ :: p.darts
+
+-- def IsHyperPath (h : H.IsWalk x W) : Prop :=
+
+-- def IsPath
+
+-- def IsHyperCycle
+
+-- def IsCycle
+
+-- def IsTrail
+
+-- def IsStrictTrail
+
+end IsWalk
 
 
 -- Walk in H is a (graph) walk in G(H), the incidence (bipartite) graph
